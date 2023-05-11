bd1b1e2fe06bbf6743f0b61a193164b568823361
Toshifumi Monma <toshifumi.monma@luxiar.com>
2023-05-11 16:24:10 +0900

    3-3-5 削除機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'tasks#index'
-  resources :tasks, only: [:index, :show, :new, :create, :edit, :update]
+  resources :tasks
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
