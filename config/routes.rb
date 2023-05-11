fa5379a9f83dc939c50b8af074d6650b706674e3
Toshifumi Monma <toshifumi.monma@luxiar.com>
2023-05-11 11:36:23 +0900

    3-3-0 ルーティングが明示的になるように変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'tasks#index'
-  resources :tasks
+  resources :tasks, only: [:index, :new, :create]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
